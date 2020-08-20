7d8d96970e9bd6008bebba3e560c6f0940929a76
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2020-08-20 16:40:25 +0200

    Adds background image to sign in page

--- src/features/signIn/components/SignIn.js
+++ src/features/signIn/components/SignIn.js
@@ -1,17 +1,55 @@
 import React from "react";
 import styled from "styled-components";
+import { Users, MessageCircle, Search } from "react-feather";
 
-const Container = styled.div`
+const MainContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
+  min-height: 100vh;
+  height: 100%;
+`;
+
+const Container = styled.div`
+  background: url("https://comm-tech.co.uk/wp-content/uploads/2018/08/OTB_Company_Blue.png.img_.fullhd.medium.png");
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Wrapper = styled.div`
+  margin: 1.4rem 0;
+  color: ${(props) => props.theme.secondary};
+  display: flex;
+  align-items: center;
+`;
+
+const ImageText = styled.p`
+  font-size: 1.4rem;
+  margin-left: 0.6rem;
 `;
 
 function SignIn() {
   return (
-    <Container>
-      <div>Picture</div>
-      <div>SignIn</div>
-    </Container>
+    <MainContainer>
+      <Container>
+        <Wrapper>
+          <Search size="1.8rem" />
+          <ImageText>Follow your interests.</ImageText>
+        </Wrapper>
+        <Wrapper>
+          <Users size="1.8rem" />
+          <ImageText>Hear what people are talking about.</ImageText>
+        </Wrapper>
+        <Wrapper>
+          <MessageCircle size="1.8rem" />
+          <ImageText>Join the conversation.</ImageText>
+        </Wrapper>
+      </Container>
+      <div></div>
+    </MainContainer>
   );
 }
 
--- src/themes/lightTheme.js
+++ src/themes/lightTheme.js
@@ -2,7 +2,7 @@ const lightTheme = {
   backgroundPrimary: "#121212",
   backgroundSecondary: "#222222",
   primary: "#f2f2f2",
-  secondary: "#B1B1B1",
+  secondary: "#fff",
 };
 
 export default lightTheme;
